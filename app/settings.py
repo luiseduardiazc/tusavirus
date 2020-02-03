d1a5576d1b9ed21c0722d716119f8f63abb738be
Luis Eduardo D <luis.diaz.car@gmail.com>
2020-02-02 20:49:08 -0500

    make configurations for static files and gunicorn

--- app/settings.py
+++ app/settings.py
@@ -110,5 +110,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-STATIC_URL = '/static/'
+MEDIA_ROOT ='media'
+MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
